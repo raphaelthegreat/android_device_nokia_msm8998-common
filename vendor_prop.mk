7ddd4256033d2f52e78a58311daba222d93c1ccf
Ady Abraham <adyabr@google.com>
2019-07-24 11:05:44 -0700

    msm8998-common: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8
    (cherry picked from commit 4e8a210c0e3e6854cd34f432f52e3f4821e7b2d5)

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -112,6 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.hardware.vulkan=adreno \
 	ro.hardware.egl=adreno \
+	debug.sf.enable_gl_backpressure=1 \
 	debug.sf.enable_hwc_vds=1 \
 	ro.vendor.display.cabl=2
 
