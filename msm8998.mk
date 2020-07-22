f369fd0958fcb1c4da50989721914a6b4abbef5e
Bharat Pawar <bpawar@codeaurora.org>
2020-07-22 20:54:03 +0530

    msm8998-common: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.
    
    Change-Id: I7529dc91c2ff994b83c7b9e7ddf4df913b9205f9

--- msm8998.mk
+++ msm8998.mk
@@ -445,11 +445,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-
 # VNDK
 # Update this list with what each blob is actually for
 # libicuuc: vendor.qti.hardware.qteeconnector@1.0-impl
