db38a41f39cf5b825fa2bedaba8eac051e98b1b7
Aayush Gupta <aayushgupta219@gmail.com>
2020-10-06 04:40:58 +0000

    msm8998-common: Cleanup graphics stack implementation
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I9ccefe9806fa4d64f3c6ef132c825023b30dcd36

--- msm8998.mk
+++ msm8998.mk
@@ -193,25 +193,27 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl:64 \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl:64 \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.renderscript@1.0-impl
+
+PRODUCT_PACKAGES += \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
     memtrack.msm8998 \
     libdisplayconfig \
     libdisplayconfig.qti.vendor \
-    libqdMetaData.system
+    libqdMetaData.system \
+    libtinyxml
 
 PRODUCT_PACKAGES += \
     vendor.display.config@2.0.vendor \
     vendor.display.config@2.0 \
-
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.renderscript@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
