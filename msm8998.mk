11fd2589a9495647f8bdb821eaca9bbe488b0c7c
George <geoster3d@gmail.com>
2020-12-02 11:18:20 +0200

    msm8998-common: Switch to sensor 1.0 HAL

--- msm8998.mk
+++ msm8998.mk
@@ -430,7 +430,6 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0 \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
