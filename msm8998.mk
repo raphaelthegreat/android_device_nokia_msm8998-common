a881d8cb5103b3a5be515c67a55204590b4017b7
George <geoster3d@gmail.com>
2020-12-04 12:24:51 +0200

    Revert "msm8998-common: Switch to sensor 1.0 HAL"
    Sensor HAL actually needs android.hardware.sensors@2.0
    This reverts commit 11fd2589a9495647f8bdb821eaca9bbe488b0c7c.

--- msm8998.mk
+++ msm8998.mk
@@ -428,6 +428,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0 \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
