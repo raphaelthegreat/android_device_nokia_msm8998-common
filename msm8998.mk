e47df1b874421b1b4989e7aa5cd49569c1a4d0a7
lucky <ah440099@gmail.com>
2021-01-15 11:32:46 +0200

    msm8998-common: Build sensorservice from source
    * Fixes following error
    Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20

--- msm8998.mk
+++ msm8998.mk
@@ -404,6 +404,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0 \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0 \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
