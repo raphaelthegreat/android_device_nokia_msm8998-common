a6d215c6dbaab2a637dacb670aaa9f5d6e3166d8
Ramii Ahmed <ramy@ahmedramy.com>
2021-03-18 00:01:55 +0000

    msm8998-common: Build mock power.stats for BatteryStatsService * Fixes the following logcat error E BatteryStatsService: Unable to load Power Hal or power.stats HAL

--- msm8998.mk
+++ msm8998.mk
@@ -341,7 +341,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.0.vendor \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
