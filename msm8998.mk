2cb0e10afc66ad841014d14ddbce42e5a684e73e
George <geoster3d@gmail.com>
2021-07-13 20:36:50 +0300

    msm8998-common: Cleanup source built camera components

--- msm8998.mk
+++ msm8998.mk
@@ -174,18 +174,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
-    Gallery2
-
-PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.device@3.4:64 \
-    android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4:64 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64 \
-    vendor.qti.hardware.camera.device@1.0:64
+    android.hardware.camera.provider@2.5 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    Snap
 
 # Component overrides
 PRODUCT_COPY_FILES += \
